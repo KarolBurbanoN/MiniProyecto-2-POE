917a8ae9e407fa634b3557ca4d6754487d2beefb
JeidyMurillo <135182663+JeidyMurillo@users.noreply.github.com>
2024-05-16 19:15:57 -0500

    Se agrega la interfaz grafica
    Se añade la interfaz grafica del primer punto

---
+++ guiFerreteria/src/InicioJPanel.java
@@ -0,0 +1,207 @@
+/**
+ * @file inicioJPanel.java
+ * @brief Este archivo contiene la primera pagina.
+ * @author
+ *         - Sebastian Castro - 2359435
+ *         - Karol Burbano - 2359305
+ *         -Veronica Mujica - 2359406
+ *         -Jeidy Murillo - 2359310 
+ */
+
+public class InicioJPanel extends javax.swing.JPanel {
+
+    /**
+     * Creates new form InicioJPanel
+     */
+    public InicioJPanel() {
+        initComponents();
+    }
+
+    //@SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabelDescripcionn3 = new javax.swing.JLabel();
+        jLabelDescripcionn4 = new javax.swing.JLabel();
+        jLabelDescripcionn6 = new javax.swing.JLabel();
+        jLabelDescripcionn7 = new javax.swing.JLabel();
+        jLabelimg = new javax.swing.JLabel();
+        jLabelDescripcionn5 = new javax.swing.JLabel();
+        jLabelDescripcionn2 = new javax.swing.JLabel();
+        jLabelTitulo = new javax.swing.JLabel();
+        jLabelDescripcionn1 = new javax.swing.JLabel();
+        jPanel2 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+
+        setBackground(new java.awt.Color(255, 255, 255));
+        setMinimumSize(new java.awt.Dimension(1008, 0));
+        setPreferredSize(new java.awt.Dimension(1008, 0));
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabelDescripcionn3.setBackground(new java.awt.Color(255, 204, 153));
+        jLabelDescripcionn3.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 14)); // NOI18N
+        jLabelDescripcionn3.setForeground(new java.awt.Color(51, 51, 51));
+        jLabelDescripcionn3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelDescripcionn3.setText("de inventarios en su ferreteria las principales");
+        jLabelDescripcionn3.setBorder(javax.swing.BorderFactory.createCompoundBorder());
+
+        jLabelDescripcionn4.setBackground(new java.awt.Color(255, 204, 153));
+        jLabelDescripcionn4.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 14)); // NOI18N
+        jLabelDescripcionn4.setForeground(new java.awt.Color(51, 51, 51));
+        jLabelDescripcionn4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelDescripcionn4.setText(" funciones son:");
+        jLabelDescripcionn4.setBorder(javax.swing.BorderFactory.createCompoundBorder());
+
+        jLabelDescripcionn6.setBackground(new java.awt.Color(255, 204, 153));
+        jLabelDescripcionn6.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 14)); // NOI18N
+        jLabelDescripcionn6.setForeground(new java.awt.Color(51, 51, 51));
+        jLabelDescripcionn6.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelDescripcionn6.setText("elimininar productos, actualizar y ver algunas");
+        jLabelDescripcionn6.setBorder(javax.swing.BorderFactory.createCompoundBorder());
+
+        jLabelDescripcionn7.setBackground(new java.awt.Color(255, 204, 153));
+        jLabelDescripcionn7.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 14)); // NOI18N
+        jLabelDescripcionn7.setForeground(new java.awt.Color(51, 51, 51));
+        jLabelDescripcionn7.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabelDescripcionn7.setText("estadisticas ");
+        jLabelDescripcionn7.setBorder(javax.swing.BorderFactory.createCompoundBorder());
+
+        jLabelimg.setIcon(new javax.swing.ImageIcon(getClass().getResource("puntero2.png"))); // NOI18N
+
+        jLabelDescripcionn5.setBackground(new java.awt.Color(255, 204, 153));
+        jLabelDescripcionn5.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 14)); // NOI18N
+        jLabelDescripcionn5.setForeground(new java.awt.Color(51, 51, 51));
+        jLabelDescripcionn5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelDescripcionn5.setText("-Poder visualizar el inventario, agregar productos");
+        jLabelDescripcionn5.setBorder(javax.swing.BorderFactory.createCompoundBorder());
+
+        jLabelDescripcionn2.setBackground(new java.awt.Color(255, 204, 153));
+        jLabelDescripcionn2.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 14)); // NOI18N
+        jLabelDescripcionn2.setForeground(new java.awt.Color(51, 51, 51));
+        jLabelDescripcionn2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelDescripcionn2.setText("Esta herramienta sera de ayuda para el manejo");
+        jLabelDescripcionn2.setBorder(javax.swing.BorderFactory.createCompoundBorder());
+
+        jLabelTitulo.setBackground(new java.awt.Color(255, 204, 153));
+        jLabelTitulo.setFont(new java.awt.Font("Yu Gothic UI Semibold", 3, 45)); // NOI18N
+        jLabelTitulo.setForeground(new java.awt.Color(51, 0, 0));
+        jLabelTitulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelTitulo.setText("Bienvenido ");
+        jLabelTitulo.setBorder(javax.swing.BorderFactory.createCompoundBorder());
+
+        jLabelDescripcionn1.setBackground(new java.awt.Color(255, 204, 153));
+        jLabelDescripcionn1.setFont(new java.awt.Font("Yu Gothic UI Semibold", 0, 14)); // NOI18N
+        jLabelDescripcionn1.setForeground(new java.awt.Color(51, 51, 51));
+        jLabelDescripcionn1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabelDescripcionn1.setText("Sistema de gestion de inventario para ferreteria.");
+        jLabelDescripcionn1.setBorder(javax.swing.BorderFactory.createCompoundBorder());
+
+        jPanel2.setBackground(new java.awt.Color(51, 51, 51));
+        jPanel2.setForeground(new java.awt.Color(51, 51, 51));
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 311, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("fondo.png"))); // NOI18N
+        jLabel1.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap(121, Short.MAX_VALUE)
+                        .addComponent(jLabelimg)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(jLabelTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, 307, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(67, 67, 67))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(52, 52, 52)
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(jLabelDescripcionn2, javax.swing.GroupLayout.PREFERRED_SIZE, 342, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jLabelDescripcionn3, javax.swing.GroupLayout.PREFERRED_SIZE, 332, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jLabelDescripcionn1, javax.swing.GroupLayout.PREFERRED_SIZE, 355, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jLabelDescripcionn5, javax.swing.GroupLayout.PREFERRED_SIZE, 342, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                            .addComponent(jLabelDescripcionn6)
+                                            .addComponent(jLabelDescripcionn7, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addGap(42, 42, 42))))
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGap(66, 66, 66)
+                                .addComponent(jLabelDescripcionn4)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 303, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(41, 41, 41)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabelTitulo, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabelimg, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(26, 26, 26)
+                .addComponent(jLabelDescripcionn1, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabelDescripcionn2, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabelDescripcionn3, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabelDescripcionn4, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(30, 30, 30)
+                .addComponent(jLabelDescripcionn5, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabelDescripcionn6, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabelDescripcionn7, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabelDescripcionn1;
+    private javax.swing.JLabel jLabelDescripcionn2;
+    private javax.swing.JLabel jLabelDescripcionn3;
+    private javax.swing.JLabel jLabelDescripcionn4;
+    private javax.swing.JLabel jLabelDescripcionn5;
+    private javax.swing.JLabel jLabelDescripcionn6;
+    private javax.swing.JLabel jLabelDescripcionn7;
+    private javax.swing.JLabel jLabelTitulo;
+    private javax.swing.JLabel jLabelimg;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    // End of variables declaration//GEN-END:variables
+}
