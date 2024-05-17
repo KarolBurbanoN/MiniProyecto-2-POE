515cf8858b681c265079e5a7650278bc35a377d6
JeidyMurillo <135182663+JeidyMurillo@users.noreply.github.com>
2024-05-16 20:12:20 -0500

    Se agrega la clase producto

---
+++ guiFerreteria/src/ProductNotFoundException.java
@@ -0,0 +1,15 @@
+/**
+ * @file ProductNotFoundException.java
+ * @brief Este archivo contiene exception.
+ * @author
+ *         - Sebastian Castro - 2359435
+ *         - Karol Burbano - 2359305
+ *         -Veronica Mujica - 2359406
+ *         -Jeidy Murillo - 2359310 
+ */
+public class ProductNotFoundException extends Exception{
+        public ProductNotFoundException(String mensaje) {
+            super(mensaje);
+        }
+}    
+
